0735d5e84b83203d6a6ffeb94647c1a977b255a8
Roman Garza <roman_garza@protonmail.com>
2022-02-27 13:44:56 -0800

    changed jquery version

--- script.js
+++ script.js
@@ -1,40 +1,40 @@
 $(document).ready(function () {
-
-    var quote;
-
-    function getNewQuote() {
-        $.ajax({
-            url: 'http://api.forismatic.com/api/1.0/',
-            jsonp: 'jsonp',
-            dataType: 'jsonp',
-            data: {
-                method: 'getQuote',
-                lang: 'en',
-                format: 'jsonp'
-            },
-            success: function (response) {
-                quote = response.quoteText;
-                $('#text').text('"' + response.quoteText + '"');
-                if (response.quoteAuthor) {
-                    $('#author').text('-' + response.quoteAuthor);
-                } else {
-                    $('author').text('- unkown');
-                }
-            }
-        });
-    }
-
-    getNewQuote();
-
-    $('.get-quote').on('click', function (e) {
-        e.preventDefault();
-        getNewQuote();
+  var quote;
+  var author;
+  function newQuote() {
+    $.ajax({
+      url: "https://api.forismatic.com/api/1.0/",
+      jsonp: "jsonp",
+      dataType: "jsonp",
+      data: {
+        method: "getQuote",
+        lang: "en",
+        format: "jsonp",
+      },
+      success: function (apiResponse) {
+        author = apiResponse.quoteAuthor;
+        quote = apiResponse.quoteText;
+        if (author) {
+          author = "said by " + author;
+          $("#author").text(author);
+        } else {
+          $("#author").text("Unknown");
+        }
+        $(".quote").text(quote);
+      },
     });
+  }
+  newQuote();
 
-    $('.share-quote').on('click', function (e) {
-        e.preventDefault();
-        window.open('https://twitter.com/intent/tweet?text=' + encodeURIComponent(quote));
-    });
+  $("#get-quote").click(function (event) {
+    event.preventDefault();
+    newQuote();
+  });
 
-
+  $("#share-quote").click(function () {
+    window.open(
+      "https://twitter.com/intent/tweet?text=" +
+        encodeURIComponent(quote + " — " + author)
+    );
+  });
 });
